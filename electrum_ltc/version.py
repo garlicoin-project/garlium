08b84fe19750c99613c13c2d0cb3e2ed75936352
pooler <pooler@litecoinpool.org>
2021-03-19 17:18:03 +0000

    Litecoin: bump version to 4.0.9.3

--- electrum_ltc/version.py
+++ electrum_ltc/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = '4.0.9.2'   # version of the client package
-APK_VERSION = '4.0.9.2'        # read by buildozer.spec
+ELECTRUM_VERSION = '4.0.9.3'   # version of the client package
+APK_VERSION = '4.0.9.3'        # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
